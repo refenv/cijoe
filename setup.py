3f7d93e3ef4b47910ae7d943dff99d1d5d862be1
Simon A. F. Lund <slund@cnexlabs.com>
2018-11-19 15:04:16 +0100

    version-bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import glob
 
 setup(
     name="cijoe",
-    version="0.0.1",
+    version="0.0.2",
     description="Tools for for development and testing",
     author="Simon A. F. Lund",
     author_email="slund@cnexlabs.com",
