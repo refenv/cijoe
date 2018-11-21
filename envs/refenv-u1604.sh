99429205d115a26b49d9813b3a094a586bc07587
Simon A. F. Lund <slund@cnexlabs.com>
2018-11-21 14:47:40 +0100

    envs: redefined env-definition for Ubuntu 16.04 reference environment

--- envs/refenv-u1604.sh
+++ envs/refenv-u1604.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Paths packages installed from source in the Ubuntu reference environment
+# Paths packages installed from source in the Ubuntu 1604 reference environment
 #
 # NOTE:
 # - fio 3.12 is installed on the system, additional versions in /opt
@@ -13,11 +13,21 @@ export RBENCH_BIN=/opt/rocksdb/db_bench
 export BLKTESTS_HOME=/opt/blktests
 export XFSTESTS_HOME=/var/lib/xfstests
 
-export FIO_2.2.8_HOME=/opt/fio-2.2.8
-export FIO_2.2.10_HOME=/opt/fio-2.2.10
+export FIO_2_2_8_HOME=/opt/fio-2.2.8
+export FIO_2_2_10_HOME=/opt/fio-2.2.10
 
 export ACT_HOME=/opt/act
 export SPDK_HOME=/opt/spdk
 export YCSB_HOME=/opt/YCSB
 
 export BLOCK_STORAGE_HOME=/opt/block-storage
+
+# Hazardous region
+#
+# As most tests will write to devices, then it is important that you make sure
+# to define them correctly to around purging your operating system installation
+# or precious data. So, default values are not provided.
+#
+#export SSH_USER=root
+#export SSH_HOST=ch-lab-01
+#export BLOCK_DEV_NAME=nvme0n1
