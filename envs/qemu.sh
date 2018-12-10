3d925728beddbe9647faf0988462b30181349f05
Simon A. F. Lund <slund@cnexlabs.com>
2018-12-10 14:04:22 +0100

    envs: fixed for changes in QEMU module and expanded refenv

---
+++ envs/qemu.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#
+# QEMU environment skeleton
+#
+# The SSH configuration is overwritten such that everything using the "ssh::"
+# module will be targeted towards the QEMU guest and to access the QEMU host and
+# explicit "QEMU_HOST" and utility functions are provided by the "qemu::" module
+#
+
+# SSH config for logging into QEMU guest
+export SSH_USER=root
+export SSH_HOST=localhost
+export SSH_PORT=2022
+
+# SSH config for logging into QEMU host
+export QEMU_HOST=localhost
+export QEMU_HOST_USER=root
+
+#
+# QEMU definitions
+#
+export QEMU_BIN="/opt/qemu-nvme/bin/qemu-system-x86_64"
+export QEMU_GUESTS="/opt/qemu-guests"
+# The guest machine
+export QEMU_GUEST_NAME=emujoe
+export QEMU_GUEST_PATH="$QEMU_GUESTS/$QEMU_GUEST_NAME"
+export QEMU_GUEST_BOOT_IMG="$QEMU_GUEST_PATH/boot.qcow2"
+export QEMU_GUEST_CONSOLE="file"
+#export QEMU_GUEST_KERNEL="$QEMU_GUEST_PATH/bzImage"
+
+# Define a virtual Open-Channel SSD
+export QEMU_NVME_ID="cij_nvme"
+
+# OCSSD Geometry
+export QEMU_NVME_LINES=100
+export QEMU_NVME_NUM_GRP=4
+export QEMU_NVME_NUM_PU=4
+export QEMU_NVME_NUM_SEC=400
+export QEMU_NVME_SEC_SIZE=4096
+export QEMU_NVME_MS=16
+#export QEMU_NVME_MDTS=7
+#export QEMU_NVME_NLBAF=6
+
+# OCSSD constraints
+export QEMU_NVME_WS_MIN=12
+export QEMU_NVME_WS_OPT=24
+export QEMU_NVME_MW_CUNITS=0
+
+# OCSSD error-injection
+#export QEMU_NVME_CHUNKTABLE="$CIJ_ROOT/testfiles/qemu/chunktable.qemu"
+#export QEMU_NVME_RESETFAIL="$CIJ_ROOT/testfiles/qemu/resetfail.qemu"
+#export QEMU_NVME_WRITEFAIL="$CIJ_ROOT/testfiles/qemu/writefail.qemu"
+
+export NVME_DEV_NAME="nvme0n1"
+export LNVM_BGN=0
+export LNVM_END=$(( $QEMU_NVME_NUM_PU - 1 ))
+export BLOCK_DEV_NAME="nvme0n1b000e00"$LNVM_END
--- envs/refenv-u1604.sh
+++ envs/refenv-u1604.sh
@@ -8,29 +8,33 @@
 #
 # See the reference environments for additional information
 #
+
+#
+# The following are paths in the TARGET used by their respective hooks, modules,
+# and testcases
+#
+
+# See module "rbench::" in modules/rbench.sh
 export RBENCH_BIN=/opt/rocksdb/db_bench
 
+# See module "blktests::" in modules/blktests.sh
 export BLKTESTS_HOME=/opt/blktests
+
+# See module "xfstests::" in modules/xfstests.sh
 export XFSTESTS_HOME=/var/lib/xfstests
 
+# See hook "spdk" in hooks/spdk.sh and hooks/spdk_exit.sh
+export SPDK_HOME=/opt/spdk
+
+# These are used by testcases that rely on specific version of fio, the "fio::"
+# module uses the fio version installed on the system
 export FIO_2_2_8_HOME=/opt/fio-2.2.8
 export FIO_2_2_10_HOME=/opt/fio-2.2.10
 
-export ACT_HOME=/opt/act
-export SPDK_HOME=/opt/spdk
+# These are defined for testcases implemented as wrappers
 export YCSB_HOME=/opt/YCSB
-
+export ACT_HOME=/opt/act
 export BLOCK_STORAGE_HOME=/opt/block-storage
-
-#
-# Sensitive Region
-#
-#export CIJ_TRGT=localhost
-#export SSH_HOST=$CIJ_TRGT
-#export SSH_USER=root
-#export IPMI_HOST=${CIJ_TRGT}-ipmi
-#export IPMI_USER=ADMIN
-#export IPMI_PASS=ADMIN
 
 #
 # Hazardous Region
@@ -42,3 +46,15 @@ export BLOCK_STORAGE_HOME=/opt/block-storage
 #export PCI_DEV_NAME=0000:01:00.0
 #export NVME_DEV_NAME=nvme0n1
 #export BLOCK_DEV_NAME=$NVME_DEV_NAME
+
+#
+# Sensitive Region
+#
+#export CIJ_TRGT=localhost
+
+#export SSH_HOST=$CIJ_TRGT
+#export SSH_USER=root
+
+#export IPMI_HOST=${CIJ_TRGT}-ipmi
+#export IPMI_USER=ADMIN
+#export IPMI_PASS=ADMIN
