633a967b9a0607e6e72d7192d209fd22e88950ac
Simon A. F. Lund <slund@cnexlabs.com>
2018-12-17 12:38:23 +0100

    hooks/spdk: added evar forward

--- hooks/spdk.sh
+++ hooks/spdk.sh
@@ -10,8 +10,18 @@ source "$CIJ_ROOT/modules/cijoe.sh"
 test::enter
 
 function hook::spdk_enter {
+  SPDK_CMD="$SPDK_HOME/scripts/setup.sh"
+  if [[ ! -z "$HUGEMEM" ]]; then
+    SPDK_CMD="HUGEMEM=$HUGEMEM $SPDK_CMD"
+  fi
+  if [[ ! -z "$NRHUGE" ]]; then
+    SPDK_CMD="NRHUGE=$NRHUGE $SPDK_CMD"
+  fi
+  if [[ ! -z "$HUGENODE" ]]; then
+    SPDK_CMD="HUGENODE=$HUGENODE $SPDK_CMD"
+  fi
 
-  if ! ssh::cmd "$SPDK_HOME/scripts/setup.sh"; then
+  if ! ssh::cmd "$SPDK_CMD"; then
     cij:err "hook::spdk_enter: FAILED: setting up SPDK devices"
     return 1
   fi
