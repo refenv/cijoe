1e90a337fad9ab0bf926f7b2cb38fb16efe1451e
Klaus B. A. Jensen <klaus.jensen@cnexlabs.com>
2019-03-13 13:16:22 +0100

    qemu: update module
    Update the qemu.sh module to support the newest version of qemu-nvme and
    clean up the reference environments.
    
    Update documentation on how to use QEMU interactively inside CIJOE.
    
    Signed-off-by: Klaus B. A. Jensen <klaus.jensen@cnexlabs.com>

--- envs/qemu.sh
+++ envs/qemu.sh
@@ -19,33 +19,37 @@
 #
 # QEMU definitions
 #
-export QEMU_BIN="/opt/qemu-nvme/bin/qemu-system-x86_64"
+
+qemu_bin_root="/opt/qemu-nvme/bin"
+export QEMU_BIN="$qemu_bin_root/qemu-system-x86_64"
+export QEMU_IMG_BIN="$qemu_bin_root/qemu-img"
 export QEMU_GUESTS="/opt/qemu-guests"
+
 # The guest machine
 export QEMU_GUEST_NAME=emujoe
 export QEMU_GUEST_PATH="$QEMU_GUESTS/$QEMU_GUEST_NAME"
 export QEMU_GUEST_BOOT_IMG="$QEMU_GUEST_PATH/boot.qcow2"
 export QEMU_GUEST_CONSOLE="file"
-export QEMU_GUEST_MEM="12GB"
+export QEMU_GUEST_MEM="4G"
 #export QEMU_GUEST_KERNEL="$QEMU_GUEST_PATH/bzImage"
+#export QEMU_GUEST_APPEND="net.ifnames=0 biosdevname=0"
 
 # OCSSD: Define a virtual Open-Channel SSD
 export QEMU_NVME_ID="cij_nvme"
 
 # OCSSD: Geometry
-export QEMU_NVME_LINES=100
-export QEMU_NVME_NUM_GRP=4
+export QEMU_NVME_NUM_GRP=2
 export QEMU_NVME_NUM_PU=4
-export QEMU_NVME_NUM_SEC=528
-export QEMU_NVME_SEC_SIZE=4096
+export QEMU_NVME_NUM_CHK=60
+export QEMU_NVME_NUM_SEC=4096
+export QEMU_NVME_LBADS=4096
 export QEMU_NVME_MS=16
 #export QEMU_NVME_MDTS=7
-#export QEMU_NVME_NLBAF=6
 
 # OCSSD: constraints
-export QEMU_NVME_WS_MIN=12
-export QEMU_NVME_WS_OPT=24
-export QEMU_NVME_MW_CUNITS=0
+export QEMU_NVME_WS_MIN=4
+export QEMU_NVME_WS_OPT=8
+export QEMU_NVME_MW_CUNITS=24
 
 # OCSSD: error-injection
 #export QEMU_NVME_CHUNKTABLE="$CIJ_ROOT/testfiles/qemu/chunktable.qemu"
