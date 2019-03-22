c8777ada6018f353bfb4a218da5aed1fbb1f09dc
Simon A. F. Lund <slund@cnexlabs.com>
2019-03-22 12:51:00 +0100

    hooks: dmesg, slight improvement

--- hooks/dmesg_enter.sh
+++ hooks/dmesg_enter.sh
@@ -15,9 +15,13 @@ hook::dmesg_enter() {
     return 1
   fi
 
-  ssh::cmd "dmesg -w -T" > $CIJ_TEST_AUX_ROOT/hook_dmesg.log &
-  if [[ $? -ne 0 ]]; then
-    cij.warn "hook::dmesg_enter: FAILED starting dmesg log err: '$?'"
+  ssh::cmd "dmesg -w -T" > "$CIJ_TEST_AUX_ROOT/hook_dmesg.log" &
+  SSH_DMESG_PID="$!"
+
+  # If the background process fails early then we can catch it here do note that
+  # in case it is timing out, then that will be a false positive
+  if ! ps -p "$SSH_DMESG_PID"; then
+    cij::warn "hook::dmesg_enter: FAILED starting dmesg log err: '$?'"
     return 1
   fi
 
