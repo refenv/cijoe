9b45c398cc54d685514befbbe121a6cd46b7869e
Simon A. F. Lund <os@safl.dk>
2019-11-13 08:04:29 +0100

    Version bump
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(*parts):
 
 setup(
     name="cijoe",
-    version="0.0.27",
+    version="0.0.28",
     description="Tools for systems development and testing",
     long_description=read('README.rst'),
     author="Simon A. F. Lund",
