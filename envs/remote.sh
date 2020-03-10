700150f1f8dae3970a0c71b98940e37cd1ec2f67
Simon A. F. Lund <simon.lund@samsung.com>
2020-03-10 10:23:15 +0100

    envs: added remote env
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ envs/remote.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# This is minimal configuration for CTRL <-> TRGT setup
+#
+# SSH key-based auth will be used to access "your-remote-host" as the user "root"
+#
+
+# Adjust the lines below to suit your environment
+export SSH_HOST=your-remote-host
+export SSH_USER=root
