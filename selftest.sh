e5284065df3bff0bee24590d614450bb6e1917cb
Niclas Hedam <niclas@hed.am>
2020-03-31 10:34:44 +0200

    selftest: fixes for osx
    Signed-off-by: Niclas Hedam <niclas@hed.am>

--- selftest.sh
+++ selftest.sh
@@ -72,8 +72,8 @@ main() {
   fi
 
   if [[ $open_reports -gt 0 ]]; then
-    xdg-open "$res_dpath/testcases.html" &
-    xdg-open "$res_dpath/report.html" &
+    (xdg-open "$res_dpath/testcases.html" || open "$res_dpath/testcases.html") &
+    (xdg-open "$res_dpath/report.html" || open "$res_dpath/report.html") &
   fi
 
   cij::info "res: '$res'"
