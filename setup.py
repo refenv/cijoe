84e8543991ac19e3c0660e8b2534a59beceae552
Simon A. F. Lund <simon.lund@samsung.com>
2020-08-13 11:53:48 +0200

    dist
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(*parts):
 
 setup(
     name="cijoe",
-    version="0.0.36.dev0",
+    version="0.0.36.dev1",
     description="Tools for systems development and testing",
     long_description=read('README.rst'),
     author="Simon A. F. Lund",
