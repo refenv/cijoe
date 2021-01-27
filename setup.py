6662f3f62afb659f97406c096f0152b56beeed2c
Simon A. F. Lund <simon.lund@samsung.com>
2021-01-27 12:43:26 +0100

    version-bump: v0.1.42
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(*parts):
 
 setup(
     name="cijoe",
-    version="0.0.38",
+    version="0.1.42",
     description="Tools for systems development and testing",
     long_description=read('README.rst'),
     author="Simon A. F. Lund",
