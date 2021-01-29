4aa01ca37daff91c1c71c410eb46ba4fad312fd5
Simon A. F. Lund <simon.lund@samsung.com>
2021-01-29 12:07:44 +0100

    Add selftesting using mypy
    Add testcase invoking mypy on cijoe Python module
    Add mypy testcase to cijoe testplan
    Add installation of mypy on GHA and Travis
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ testcases/cijoe_mypy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Run mypy on Python source code found in $CIJ_PKG_REPOS
+#
+# This testcase runs mypy to check for possible issues in the cijoe data structures, specifically
+# the representation of testrun, testcases, testsuites, and hooks.
+#
+# shellcheck disable=SC2119
+#
+CIJ_TEST_NAME=$(basename "${BASH_SOURCE[0]}")
+export CIJ_TEST_NAME
+# shellcheck source=modules/cijoe.sh
+source "$CIJ_ROOT/modules/cijoe.sh"
+test::enter
+
+cij::info "CIJ_PKG_REPOS: '${CIJ_PKG_REPOS}'"
+
+if [[ -z "$CIJ_PKG_REPOS" ]]; then
+  test::fail "Please set 'CIJ_PKG_REPOS'"
+fi
+
+# shellcheck disable=SC2086
+if ! mypy "${CIJ_PKG_REPOS}/modules/cij" ; then
+  popd || true
+  test::fail
+fi
+
+test::pass
