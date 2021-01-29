8a66f6d4b8f5702ebb54ad6bd9d9ff60e55c4404
Simon A. F. Lund <simon.lund@samsung.com>
2021-01-29 12:07:20 +0100

    Add sefltesting using flake8
    Add testcase invoking flake8 on cijoe Python module
    Add flake8 testcase to cijoe testplan
    Add installation of flake8 on GHA and Travis
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ testcases/cijoe_flake8.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# Run flake8 on Python source code found in $CIJ_PKG_REPOS
+#
+# This testcase runs the flake8 Python language integrity on all the Python code it can find it
+# $CIJ_PKG_REPOS. The intent is to use this on CIJOE as well as CIJOE packages.
+#
+# shellcheck disable=SC2119
+#
+CIJ_TEST_NAME=$(basename "${BASH_SOURCE[0]}")
+export CIJ_TEST_NAME
+# shellcheck source=modules/cijoe.sh
+source "$CIJ_ROOT/modules/cijoe.sh"
+test::enter
+
+cij::info "CIJ_PKG_REPOS: '${CIJ_PKG_REPOS}'"
+
+if [[ -z "$CIJ_PKG_REPOS" ]]; then
+  test::fail "Please set 'CIJ_PKG_REPOS'"
+fi
+pushd "$CIJ_PKG_REPOS" || test::fail "Invalid 'CIJ_PKG_REPOS'"
+
+paths=""
+for path in {bin,hooks,testcases}/* "${CIJ_PKG_REPOS}/modules/cij/"*.py "${CIJ_PKG_REPOS}/setup.py"; do
+  if [[ -d "$path" ]]; then
+    continue
+  fi
+
+  if grep "^#.*python" "$path" > /dev/null || [[ $path == *.py ]]; then
+    paths="$paths $path"
+  fi
+done
+
+if [[ -z "$paths" ]]; then
+  cij::warn "No Python source to check"
+  popd || true
+  test::pass
+fi
+
+# shellcheck disable=SC2086
+if ! flake8 $paths ; then
+  popd || true
+  test::fail
+fi
+
+popd || true
+test::pass
