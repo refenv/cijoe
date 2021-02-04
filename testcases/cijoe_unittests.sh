4151540deda3f2f68eccd85c34ca456dd0ab7f92
Michael Bang <mi.bang@samsung.com>
2021-02-04 10:04:35 +0100

    testplans: add cijoe_unittests to cijoe.plan
    Signed-off-by: Michael Bang <mi.bang@samsung.com>

---
+++ testcases/cijoe_unittests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Run the CIJOE unittests
+#
+# This runs CIJOE unittest of cijoe itself via module execution, e.g. running the unittests
+# packages with CIJOE.
+#
+# shellcheck disable=SC2119
+#
+CIJ_TEST_NAME=$(basename "${BASH_SOURCE[0]}")
+export CIJ_TEST_NAME
+# shellcheck source=modules/cijoe.sh
+source "$CIJ_ROOT/modules/cijoe.sh"
+test::enter
+
+# shellcheck disable=SC2086
+if ! python3 -m cij.unittests; then
+  test::fail
+fi
+
+test::pass
