b36c8067701f827bed8aa22b628afddb2b2c93f4
Simon A. F. Lund <simon.lund@samsung.com>
2021-02-10 11:14:09 +0100

    mod:py: add configuration class
    The different tools require that CIJOE environment variables are
    set; CIJ_ROOT, CIJ_TESTCASES, CIJ_TEMPLATES, etc. They also each do
    their own parsing to setup a conf-dict.
    
    This introduces a Config-class to hold these paths and a
    factory-function to contruct it. The factory function inspects the
    environment variables, and when they are not set, constructs the paths
    based on the root provided by ``cij_root``.
    
    This will allow for tools to run without having to start the interactive
    cijoe-mode first.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

---
+++ modules/cij/conf.py
@@ -0,0 +1,72 @@
+"""
+   CIJOE Configuration
+"""
+from subprocess import Popen, PIPE
+from typing import Optional
+import dataclasses
+import os
+import cij
+
+
+CFG_FIELDS = [
+    "root", "envs", "modules", "hooks", "testfiles",
+    "templates", "testplans", "testsuites", "testcases"
+]
+
+
+@dataclasses.dataclass
+class Config:
+    """Contains absolute paths to CIJOE entities"""
+    # pylint: disable=too-many-instance-attributes
+    # This is a dataclass, many instance attributes are expected.
+
+    root: str = ""          # Path to root of CIJOE installation
+    envs: str = ""          # Path to environment-definitions
+    hooks: str = ""         # Path to hooks
+    modules: str = ""       # Path to Bash modules
+    templates: str = ""     # Path to reporter-templates
+    testcases: str = ""     # Path to testcase scripts (.sh)
+    testfiles: str = ""     # Path to testfiles e.g. fio scripts etc.
+    testplans: str = ""     # Path to testplans definitions (.plan)
+    testsuites: str = ""    # Path to testsuite definitions (.suite)
+
+
+# pylint: disable=unsubscriptable-object
+def from_system() -> Optional[Config]:
+    """
+    Config-factory; producing a Config based on environment variables and when
+    environment variables aren't set, fall back to the ``cij_root`` helper.
+    """
+
+    conf = Config()
+
+    # Setup configuration using environment variable definitions
+    paths_from_evars = cij.paths_from_env(
+        "CIJ",
+        [f.upper() for f in CFG_FIELDS]
+    )
+
+    missing = False
+    for key, value in paths_from_evars.items():
+        if value is None:
+            missing = True
+            break
+        setattr(conf, key.lower(), value)
+
+    if not missing:
+        return conf
+
+    # Setup configuration using 'cij_root'
+    proc = Popen(["cij_root"], stdout=PIPE)
+    out, _ = proc.communicate()
+    if proc.returncode:
+        return None
+
+    cij_root = out.decode("utf-8").strip()
+    if not os.path.exists(cij_root):
+        return None
+
+    for field in CFG_FIELDS:
+        setattr(conf, field, os.path.join(cij_root, field))
+
+    return conf
