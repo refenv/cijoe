34bcf1dd1f72f69b44eded3876d09627e59d33da
Simon A. F. Lund <simon.lund@samsung.com>
2021-02-12 12:37:10 +0100

    selftest: add extractor and analyser to selftest
    Currently this is not exercising the extractor or the analyser, however,
    it does atleast invoke them checking their CLIs are usable as expected.
    
    Signed-off-by: Simon A. F. Lund <simon.lund@samsung.com>

--- selftest.sh
+++ selftest.sh
@@ -59,6 +59,18 @@ main() {
     res=$(( res + 1 ))
   fi
 
+  # Extract metrics
+  if ! cij_extractor --extractor fio_json_read --output "$res_dpath"; then
+    cij::err "cij_extractor encountered an error"
+    res=$(( res + 1 ))
+  fi
+
+  # Analyse metrics
+  if ! cij_analyser --preqs "${CIJ_TESTFILES}/example.preqs" "$res_dpath"; then
+    cij::err "cij_analyser encountered an error"
+    res=$(( res + 1 ))
+  fi
+
   # Create test report
   if ! cij_reporter "$res_dpath"; then
     cij::err "cij_reporter encountered an error"
