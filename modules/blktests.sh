fd728d571c480aaa50f045249eb2cbbb86ca4660
Simon A. F. Lund <os@safl.dk>
2021-10-07 14:10:30 +0200

    mod:blktests: replace '::' with '.'
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- modules/blktests.sh
+++ modules/blktests.sh
@@ -4,8 +4,8 @@
 #
 # Helper functions provided to aid running in CTRL <-> TARGET setup
 #
-# blktests::env         - Checks environment for variable dependencies
-# blktests::run         - Run blktests targeting $BLOCK_DEV_PATH
+# blktests.env         - Checks environment for variable dependencies
+# blktests.run         - Run blktests targeting $BLOCK_DEV_PATH
 #
 # Variables REQUIRED by module
 #
@@ -13,19 +13,19 @@
 # BLKTESTS_HOME         - Path to blktests installation
 #
 
-blktests::env() {
-  if ! ssh::env; then
-    cij::err "blktests::env - Invalid SSH ENV."
+blktests.env() {
+  if ! ssh.env; then
+    cij.err "blktests.env - Invalid SSH ENV."
     return 1
   fi
 
   # Check for EVAR required by package
   if [[ -z "$BLOCK_DEV_PATH" ]]; then
-    cij::err "blktests::env BLOCK_DEV_PATH is not defined"
+    cij.err "blktests.env BLOCK_DEV_PATH is not defined"
     return 1
   fi
   if [[ -z "$BLKTESTS_HOME" ]]; then
-    cij::err "blktests::env BLKTESTS_HOME is not defined"
+    cij.err "blktests.env BLKTESTS_HOME is not defined"
     return 1
   fi
 
@@ -39,50 +39,50 @@ blktests::env() {
 # * results stored in temp folder (/tmp/blktests.XXXXX) on TRGT
 # * results pulled from TRGT tempt folder to CTRL folder post run
 #
-# blktests::run PATH [AUX]
+# blktests.run PATH [AUX]
 #
-blktests::run() {
-  if ! blktests::env; then
-    cij::err "blktests::run - Invalid ENV."
+blktests.run() {
+  if ! blktests.env; then
+    cij.err "blktests.run - Invalid ENV."
     return 1
   fi
 
   # Path to blktests results
   BLKTESTS_CTRL_OUTPUT=$1
   if [[ -z "$BLKTESTS_CTRL_OUTPUT" ]]; then
-    cij::err "blktests::run please provide path to result directory on CTRL"
+    cij.err "blktests.run please provide path to result directory on CTRL"
     return 1
   fi
   if [[ ! -d "$BLKTESTS_CTRL_OUTPUT" ]]; then
-    cij::err "blktests::run invalid path to result directory on CTRL"
+    cij.err "blktests.run invalid path to result directory on CTRL"
     return 1
   fi
 
   # Optional auxilary arguments to blktests ./check
   BLKTESTS_CMD_AUX=$2
 
-  cij::info "Creating result directory on TRGT"
-  BLKTESTS_TRGT_OUTPUT=$(ssh::cmd_output "mktemp -d blktests_XXXXXX -p /tmp")
-  if ! ssh::cmd "[[ -d $BLKTESTS_TRGT_OUTPUT ]]"; then
-    cij::err "blktests::run failed creating result directory on TRGT"
+  cij.info "Creating result directory on TRGT"
+  BLKTESTS_TRGT_OUTPUT=$(ssh.cmd_output "mktemp -d blktests_XXXXXX -p /tmp")
+  if ! ssh.cmd "[[ -d $BLKTESTS_TRGT_OUTPUT ]]"; then
+    cij.err "blktests.run failed creating result directory on TRGT"
     return 1
   fi
 
-  cij::info "Starting blktests"
+  cij.info "Starting blktests"
   BLKTESTS_CMD="TEST_DEVS=$BLOCK_DEV_PATH ./check"
   BLKTESTS_CMD="$BLKTESTS_CMD -d"
   BLKTESTS_CMD="$BLKTESTS_CMD --output=$BLKTESTS_TRGT_OUTPUT"
   BLKTESTS_CMD="$BLKTESTS_CMD $BLKTESTS_CMD_AUX"
 
-  ssh::cmd "cd $BLKTESTS_HOME && $BLKTESTS_CMD"
+  ssh.cmd "cd $BLKTESTS_HOME && $BLKTESTS_CMD"
   CHECK_RCODE=$?
   if [[ "$CHECK_RCODE" -ne 0 ]]; then
-    cij::err "blktests::run ./check exited with error"
+    cij.err "blktests.run ./check exited with error"
   fi
 
-  cij::info "Pulling blktests results from TRGT"
-  if ! ssh::pull "$BLKTESTS_TRGT_OUTPUT" "$BLKTESTS_CTRL_OUTPUT"; then
-    cij::err "blktests::run failed pulling results from TRGT"
+  cij.info "Pulling blktests results from TRGT"
+  if ! ssh.pull "$BLKTESTS_TRGT_OUTPUT" "$BLKTESTS_CTRL_OUTPUT"; then
+    cij.err "blktests.run failed pulling results from TRGT"
     return 1
   fi
 
