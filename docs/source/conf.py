c8e14318935f13464f2b2d623f81f2b99552daaa
Simon A. F. Lund <os@safl.dk>
2022-09-20 10:09:14 +0200

    ver: bump docs to v0.9.6
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import sphinx_rtd_theme  # noqa
 project = "cijoe"
 copyright = "2022, Simon A. F. Lund"
 author = "Simon A. F. Lund"
-release = "0.5.0"
+release = "0.9.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
