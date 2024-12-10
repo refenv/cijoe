0726791a822765c1df22a26f6580cec92bfb8848
Simon A. F. Lund <os@safl.dk>
2024-12-10 14:40:37 +0100

    feat: add 'system_imaging' package
    This prepares for separating QEMU guest control from the creation
    of .qcow2 boot images. Decoupling these components makes the
    qemu-wrapper module less dependent on boot and cloud images, allowing
    it to be refactored to operate independently of the cijoe configuration
    file. This change enables controlling QEMU via custom logic in start/
    stop scripts instead of relying on configuration file conventions.
    
    Additionally, it encapsulates various system images, including their
    origin locations, deployment locations, and the scripts required to
    build them.
    
    Also, the default example of using the package, as emitted by:
    
      cijoe --example system_imaging
    
    Is exercised via ci, this practice is what we aim for going forward, to
    ensure proper testing of cijoe itself.
    
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

---
+++ src/cijoe/system_imaging/scripts/dockerimage_from_diskimage.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+"""
+Create docker image using a disk image
+======================================
+
+This will produce dockers images for all the system images described in config. section:
+
+* ``system_imaging.images``
+
+You can reduce this by providing a case-incensitive fnmatch pattern as input to the
+script via workflow, such as these::
+
+    # This will build all images
+    with:
+      pattern: "*"
+
+    # This will build all those starting with alpine
+    with:
+      pattern: "alpine*"
+
+Retargetable: False
+-------------------
+
+This script only runs on the iniator; due to the use of 'shutil', 'download' etc.
+"""
+import logging as log
+import shutil
+from fnmatch import fnmatch
+from pathlib import Path
+
+from cijoe.core.misc import download
+from cijoe.core.resources import get_resources
+
+
+def dockerimage_from_diskimage(cijoe, image):
+    resources = get_resources()
+
+    err, state = cijoe.run_local("mktemp -d")  # Create temporary directory
+    if err:
+        log.error("Failed creating workdir")
+        return
+
+    workdir = Path(state.output().strip())
+    mountpoint = workdir / "mount"
+
+    shutil.copyfile(
+        str(resources.get("auxiliary", {}).get("system_imaging.Dockerfile").path),
+        str(workdir / "Dockerfile"),
+    )
+    shutil.copyfile(
+        str(resources.get("auxiliary", {}).get("system_imaging.dockerignore").path),
+        str(workdir / ".dockerignore"),
+    )
+
+    err, _ = cijoe.run_local(f'mkdir -p "{mountpoint}"')
+
+    commands = [
+        f"guestmount -a {image['disk']['path']} -i --ro {mountpoint}",
+        f"docker build -t {image['docker']['name']}:{image['docker']['tag']} -f {workdir}/Dockerfile {mountpoint}",
+        f"guestunmount {mountpoint}",
+    ]
+    for command in commands:
+        err, _ = cijoe.run_local(command)
+        if err:
+            log.error(f"command({command}); err({err})")
+            return err
+
+    cijoe.run_local(f'echo "Needs cleanup!" && find {workdir}')
+    cijoe.run_local(
+        f'echo "Run with: docker run -it {image['docker']['name']}:{image['docker']['tag']} bash"'
+    )
+
+    return 0
+
+
+def main(args, cijoe, step):
+    """Create a docker image using the content of a .qcow2 image"""
+
+    pattern = step.get("with", {}).get("pattern", None)
+    if pattern is None:
+        log.error("missing step-argument: with.pattern")
+        return 1
+
+    log.info(f"Got pattern({pattern})")
+
+    entry_name = "system-imaging.images"
+    images = cijoe.getconf(entry_name, {})
+    if not images:
+        log.error(f"missing: '{entry_name}' in configuration file")
+        return 1
+
+    count = 0
+    for image_name, image in cijoe.getconf("system-imaging.images", {}).items():
+        if not fnmatch(image_name.lower(), pattern.lower()):
+            log.info(f"image_name({image_name}); did not match pattern({pattern}")
+            continue
+
+        log.info(f"image_name({image_name}); matched pattern({pattern})")
+
+        err = dockerimage_from_diskimage(cijoe, image)
+        if err:
+            log.error(f"failed dockerimage_from_diskimage(); err({err})")
+            return err
+
+        count += 1
+
+    if not count:
+        log.error(f"did not build anything, count({count}); invalid with.pattern?")
+        return err
+
+    return 0
