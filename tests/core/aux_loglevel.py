23681e219d72a03d7a8e825515c60dede7a04a14
Nadja Brix Koch <n.koch@samsung.com>
2025-03-13 09:49:24 +0100

    feat(tests/cli): add tests for log level
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

---
+++ tests/core/aux_loglevel.py
@@ -0,0 +1,11 @@
+import logging as log
+
+
+def main(args, cijoe):
+    log.critical("critical")
+    log.error("error")
+    log.warning("warning")
+    log.info("info")
+    log.debug("debug")
+
+    return 0
